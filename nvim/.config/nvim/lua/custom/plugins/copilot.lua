26665a09ca856bb52df9df92bbd58d49146c2ca4
Patrick Gross <patrick.gross@de.bosch.com>
2024-08-21 13:54:52 +0200

    removed conflicting mapping

--- nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -10,7 +10,7 @@ return {
     })
   end,
   keys = {
-    { mode = 'i', '<esc>', '<Plug>(copilot-dismiss)',     desc = 'Dismiss copilot suggestion' },
+    -- { mode = 'i', '<esc>', '<Plug>(copilot-dismiss)',     desc = 'Dismiss copilot suggestion' },
     { mode = 'i', '<M-ä>', '<Plug>(copilot-next)',        desc = 'Next suggestion' },
     { mode = 'i', '<M-ö>', '<Plug>(copilot-previous)',    desc = 'Previous suggestion' },
     { mode = 'i', '<M-c>', '<Plug>(copilot-suggest)',     desc = 'Suggest completion' },
